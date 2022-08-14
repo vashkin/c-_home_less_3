97532afe3a8367de842a749dc7a438f9e0f97cb9
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-14 16:50:49 +0400

    add function Cube, add check number>0

--- task 21/Program.cs
+++ task 21/Program.cs
@@ -2,5 +2,7 @@
 double num = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
 return num;
 }
-Console.WriteLine("Растояние между обьектами: " + Distance3DObject (3,6,8,2,1,-7));
-Console.WriteLine("Растояние между обьектами: " + Distance3DObject (7,-5,0,1,-1,9));
+Console.WriteLine("Растояние между обьектами: " + Distance3DObject (3,6,8,
+                                                                    2,1,-7));
+Console.WriteLine("Растояние между обьектами: " + Distance3DObject (7,-5,0,
+                                                                    1,-1,9));
--- task 23/Program.cs
+++ task 23/Program.cs
@@ -1,3 +1,17 @@
-﻿double num =5;
-double l=Math.Pow(num,3);
-Console.WriteLine(l);
+﻿double Cube (int num){
+    double i = 1;
+    double Out = 0;
+    if(num>=1){
+    do {
+        Out=Math.Pow(i,3);
+        Console.WriteLine(Out);
+        i++;
+    }while(i<=num);
+    }
+    return Out;
+}
+
+Console.Write("Введите число: \n");
+int N = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Таблица кубов чисел от 1 до " + N + ":\n");
+Cube(N);
