1071ddff2434f1fdb37db1988751d56c90a9fd90
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-14 16:07:12 +0400

    add func distance object

--- task 21/Program.cs
+++ task 21/Program.cs
@@ -1,11 +1,6 @@
-﻿double y1 = 3;
-double x1 = 6;
-double z1 = 8;
-
-double y2 = 2;
-double x2 = 1;
-double z2 = -7;
-
-double num=0;
-num = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
-Console.WriteLine(num);
+﻿double Distance3DObject (double y1, double x1, double z1, double y2, double x2, double z2){
+double num = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+return num;
+}
+Console.WriteLine("Растояние между обьектами: " + Distance3DObject (3,6,8,2,1,-7));
+Console.WriteLine("Растояние между обьектами: " + Distance3DObject (7,-5,0,1,-1,9));
