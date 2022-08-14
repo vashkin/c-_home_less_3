00047f2738b1b37db0914c91335f0eb035ab562d
Maxim Vashkin <vashkin.m@ya.ru>
2022-08-14 15:39:14 +0400

    add formula

---
+++ task 21/Program.cs
@@ -0,0 +1,11 @@
+﻿double y1 = 3;
+double x1 = 6;
+double z1 = 8;
+
+double y2 = 2;
+double x2 = 1;
+double z2 = -7;
+
+double num=0;
+num = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+Console.WriteLine(num);
